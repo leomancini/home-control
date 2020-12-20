97d7d825c4a280a458b2a2ee3737a104e1424e88
Leo Mancini <mancini@leo.gd>
2020-12-19 18:14:02 -0800

    Create outlets.php

---
+++ natural-language-commands/outlets.php
@@ -0,0 +1,32 @@
+<?php
+	$outletCodes = Array(
+		"1" => Array(
+			"on" => 1381683,
+			"off" => 1381692
+		),
+		"2" => Array(
+			"on" => 1381827,
+			"off" => 1381836
+		),
+		"3" => Array(
+			"on" => 1382147,
+			"off" => 1382156
+		),
+		"4" => Array(
+			"on" => 1383683,
+			"off" => 1383692
+		),
+		"5" => Array(
+			"on" => 1389827,
+			"off" => 1389836
+		)
+	);
+	
+    $outletLabels = Array(
+        "diningRoomLight" => 1,
+        "livingRoomReadingLight" => 2,
+        "bedroomLight" => 3,
+		"livingRoomLight" => 4,
+		"bedroomDeskLight" => 5
+	);
+?>
